1b763bd45607668cfc104925010e902d5824d5dc
Helena <inger.helena@gmail.com>
2020-07-27 15:25:01 +0100

    created input for newChore

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
         data: {
-            toDoList: ['Food shop', 'Homework', 'Walk the dogs']
+            toDoList: ['Food shop', 'Homework', 'Walk the dogs'],
+            newChore: ''
+        },
+        methods: {
+            saveNewChore: function () {
+                this.toDoList.push(this.newChore);
+                this.newChore = ''
+            }
         }
     });
 });
